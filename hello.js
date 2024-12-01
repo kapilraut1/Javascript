ebbaadd1c4aff6ccd95f30a5cad02aba9f851165
Kapil Raut <kapilraut026@gmail.com>
2024-12-01 22:33:27 +0545

    Hello

---
+++ hello.js
@@ -0,0 +1,8 @@
+let i=19;
+let h= prompt("Please Enter a number");
+if(h==i){
+    console.log("You have enter the correct number");
+}
+else{
+    console.log("Enter the correct one. Try again");
+}
