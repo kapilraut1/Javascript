129947581f5088347839a9ee64eb5b018687a056
Kapil Raut <kapilraut026@gmail.com>
2024-12-02 22:11:34 +0545

    First release of Hello World!

---
+++ string.js
@@ -0,0 +1,12 @@
+/* let help ="HELLO";
+
+for(let i=0; i<help.length; i++)
+{
+    console.log(help[i]);
+}
+    */
+   let a ="Champion";
+console.log('I am Kapil Raut. I have been learning since ages\n so')
+console.log(`I am Kapil Raut. I have been learning since ages/n so`)
+str = "Hello\nWorld";
+console.log(str);
