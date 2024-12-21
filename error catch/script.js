65d9296fa90e408cad17b1fc293dfd3ec0de6760
Kapil Raut <kapilraut026@gmail.com>
2024-12-21 22:00:30 +0545

    trycatch

--- error catch/script.js
+++ error catch/script.js
@@ -1,6 +1,22 @@
 let a = prompt("Enter a number");
 let b= prompt("Enter a number");
-
+if(isNaN(a)|| isNaN(b)){
+    throw SyntaxError("hola the error is there");
+}
 let sum= parseInt(a) + parseInt(b);
-console.log(sum);
-alert(sum);
+//console.log(sum);
+// alert(sum);
+let x=3;
+function main(){
+try {
+    console.log("The sum is", sum*x);
+    return true;
+} catch (error) {
+    console.log("The problem is", error);
+    return false;
+}
+finally{
+    console.log("The country is great.")
+}
+}
+let d= main();
